12b116a27707278b0f2dd02ad3ee50c6d8b60d6d
Eric Sauer <ericmsauer@gmail.com>
2013-11-04 14:20:24 -0600

    Lastest before update

--- routes.js
+++ routes.js
@@ -19,6 +19,15 @@ function set_routes(app){
     { title : 'Home' }
     )
   })
+  app.get('/facebook', function(req, res){
+   res.redirect('http://www.facebook.com/EricMSauer')
+  })
+  app.get('/twitter', function(req, res){
+   res.redirect('http://www.twitter.com/EricMSauer')
+  })
+  app.get('/google', function(req, res){
+   res.redirect('http://www.plus.google.com/108773096190491439348')
+  })
 }
 
 exports.set_routes = set_routes
